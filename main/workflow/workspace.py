6a2dab1cdefb490e99bbfa2f467819ed1dae5b3d
jean-baptisteheurtel <jb.heurtel@gmail.com>
2022-05-29 14:56:25 +0200

    removing initialize method, various improvements

--- main/workflow/workspace.py
+++ main/workflow/workspace.py
@@ -1,57 +1,73 @@
 import os
+import shutil
+
 import yaml
 
 from toolbox.config import get_config
-from toolbox.util import NestedDictValues
+from toolbox.util import Map, NestedDictValues
 
 from main.parameters.base import ParamLoader
 
 
 class WorkSpace:
-    def __init__(self, params: ParamLoader):
+    def __init__(self, params: ParamLoader, reset=False):
 
         # create a config file.
-        self.gen_conf = get_config()
-        self.params = params.to_dict()
+        self.conf = get_config()
+        self.params = params.to_Map()
         self.name = self.params["nickname"]
-        self.root = os.path.join(self.gen_conf["ws"], self.name)
-        self.conf_path = os.path.join(self.root, "conf.yaml")
+        self.root = os.path.join(self.conf["workspace"], self.name)
+        # self.conf_path = os.path.join(self.root, "conf.yaml")
+        self.p = Map()
+        self.p["root"] = self.root
 
-    def initialize(self):
-        self._define_paths()
+        if reset:
+            self._reset()
+
         self._build_root()
-        self._build_fldr_structure()
-        self._init_configs()
-        self.save_configs(self.conf)
+        self._build_folder_structure()
+
+    def _mkdir(self, dir_name):
+        path = os.path.join(self.root, dir_name)
+        if not os.path.exists(path):
+            os.mkdir(os.path.join(self.root, dir_name))
+            self.p[dir_name] = path
+
+    def _reset(self):
+        if os.path.exists(self.root):
+            shutil.rmtree(self.root)
+
+    def _build_folder_structure(self):
+        self.p["data"] = os.path.join(self.p["root"], "data")
+        self.p["data_imports"] = os.path.join(self.p["data"], "import")
+        self.p["data_transformed"] = os.path.join(self.p["data"], "transformed")
+        self.p["model"] = os.path.join(self.p["root"], "model")
+        self.p["results"] = os.path.join(self.p["root"], "results")
+
+        for path in self.p.values():
+            if not os.path.exists(path):
+                os.mkdir(path)
+                print("building: " + os.path.basename(path))
+            else:
+                print("not building: " + os.path.basename(path) + " - already there")
 
     def get_config(self):
         with open(self.conf_path) as file:
             conf = yaml.load(file, Loader=yaml.Loader)
         return conf
 
-    def _define_paths(self):
-        conf = get_config()
-        self.paths = dict()
-        self.paths["ws_root"] = os.path.join(conf.ws, self.name)
-        self.paths["data"] = dict()
-        self.paths["data"]["root"] = os.path.join(self.paths["ws_root"], "data")
-        for i in ("train", "validation", "test"):
-            self.paths["data"][i] = dict()
-            self.paths["data"][i]["root"] = os.path.join(self.paths["ws_root"], "data", i)
-            self.paths["data"][i]["images"] = os.path.join(self.paths["data"][i]["root"], "images")
-        self.paths["model"] = os.path.join(self.paths["ws_root"], "model")
-        self.paths["results"] = os.path.join(self.paths["ws_root"], "results")
+    def import_data(self):
+
+        for img_set in self.params["data_sets"]:
+            source = os.path.join(self.conf.data_annotated, img_set)
+            destination = os.path.join(self.p["data_imports"], img_set)
+            print("copying: " + img_set)
+            shutil.copytree(source, destination)
+            print("finished copying: " + img_set)
 
     def _build_root(self):
-        if not os.path.exists(self.paths["ws_root"]):
-            os.mkdir(self.paths["ws_root"])
-
-    def _build_fldr_structure(self):
-        dir_list = list(NestedDictValues(self.paths))
-        for dir in dir_list:
-            if not os.path.exists(dir):
-                os.mkdir(dir)
-                print("creating directory: " + dir)
+        if not os.path.exists(self.p["root"]):
+            os.mkdir(self.p["root"])
 
     def _init_configs(self):
         conf = dict()
