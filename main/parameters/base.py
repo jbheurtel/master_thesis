313465d9f4c5701d58ae1f315f1cac5dc9637182
jean-baptisteheurtel <jb.heurtel@gmail.com>
2022-05-29 14:54:01 +0200

    few modifications: addition of label keys and label map dict info, param is now a Map()

--- main/parameters/base.py
+++ main/parameters/base.py
@@ -4,6 +4,7 @@ import yaml
 import pandas as pd
 
 from toolbox.config import get_config
+from toolbox.util import Map
 
 from main.parameters import __version__
 
@@ -12,10 +13,11 @@ class ParamLoader:
     def __init__(self, id):
         # loading parameter set
         conf = get_config()
-        param_file = conf.param + "_" + __version__ + ".xls"
+        param_file = os.path.join(conf.param, "param_" + __version__ + ".xls")
         params = pd.read_excel(param_file, sheet_name='main')
-        data_keys = pd.read_excel(param_file, sheet_name='data_key')
-        split_keys = pd.read_excel(param_file, sheet_name='split_key')
+        data_keys = pd.read_excel(param_file, sheet_name='data_key', index_col=0)
+        split_keys = pd.read_excel(param_file, sheet_name='split_key', index_col=0)
+        label_keys = pd.read_excel(param_file, sheet_name='label_key', index_col=0)
 
         params = params.set_index("id")
         param_line = params.loc[int(id)]
@@ -25,35 +27,40 @@ class ParamLoader:
         self.nickname = param_line.loc["nickname"]
         self.data_key = param_line.loc["data_key"]
         self.split_key = param_line.loc["split_key"]
-        self.group_flooded = bool(param_line.loc["group_flooded"])
+        self.label_key = None if param_line.loc["label_key"] == 0 else param_line.loc["label_key"]
         self.model = param_line.loc["model"]
 
         # interpreting keys
-        dsets = data_keys.set_index("key").loc[self.data_key]["dataset"]
+        dsets = data_keys.loc[self.data_key]["dataset"]
         if isinstance(dsets, str):
             self.data_sets = [dsets]
         else:
             self.data_sets = list(dsets)
 
-        self.split_params = split_keys.set_index("key").loc[self.split_key].set_index("part")["value"].to_dict()
+        self.split_params = split_keys.loc[self.split_key].set_index("part")["value"].to_dict()
         self.split_params = {k: v / sum(self.split_params.values()) for k, v in self.split_params.items()}
 
-    def to_dict(self):
-        params = dict()
+        if self.label_key is not None:
+            self.label_map_dict = label_keys.loc[self.label_key].set_index("original_name")["group"].to_dict()
+        else:
+            self.label_map_dict = dict()
+
+    def to_Map(self):
+        params = Map()
         params["id"] = self.id
         params["nickname"] = self.nickname
         params["data_key"] = self.data_key
         params["split_key"] = self.split_key
-        params["group_flooded"] = self.group_flooded
+        params["label_key"] = self.label_key
         params["model"] = self.model
         params["split_params"] = self.split_params
         params["data_sets"] = self.data_sets
+        params["label_map_dict"] = self.label_map_dict
         return params
 
     def get_root(self):
         conf = get_config()
         return os.path.join(conf["ws"], self.nickname)
-
 
 
 def get_params(ws_root):
