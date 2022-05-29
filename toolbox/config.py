4f217ee9eb33314d76488ce74b11c23b2ca540ba
jean-baptisteheurtel <jb.heurtel@gmail.com>
2022-05-29 14:54:32 +0200

    configs made on the spot and not pure hardcode

--- toolbox/config.py
+++ toolbox/config.py
@@ -1,17 +1,34 @@
 import os
-import yaml
-import getpass
 from toolbox.util import Map
 
 
 def get_config():
-    usr = getpass.getuser()
-    thesis_fldr = os.path.join('/Users', usr, 'Documents/thesis')
-    config_file = os.path.join(thesis_fldr, "config.yaml")
-    with open(config_file) as file:
-        conf = yaml.full_load(file)
 
-    data = Map(conf["data"])
-    conf = Map(conf)
-    conf.data = data
+    key_word = "Thesis"
+    paths = dict()
+
+    cwd = os.getcwd()
+    paths["main"] = cwd[:(cwd.rfind(key_word) + len(key_word))]
+
+    subs = {i for i in os.listdir(paths["main"]) if i[2] == "_"}
+    fldrs_main = {"".join([ch for ch in i if ch.isalpha()]).lower() : i for i in subs}
+
+    paths["data"] = os.path.join(paths["main"], fldrs_main["data"])
+
+    data_subs = {i for i in os.listdir(paths["data"]) if i[2] == "_"}
+    fldrs_data = {"".join([ch for ch in i if ch.isalpha()]).lower() : i for i in data_subs}
+
+    paths["data_annotated"] = os.path.join(paths["data"], fldrs_data["annotated"])
+    paths["data_non_annotated"] = os.path.join(paths["data"], fldrs_data["nonannotated"])
+    paths["data_transformed"] = os.path.join(paths["data"], fldrs_data["annotatedtransformed"])
+
+    paths["workspace"] = os.path.join(paths["main"], fldrs_main["workspace"])
+    paths["model"] = os.path.join(paths["main"], fldrs_main["pretrainedmodels"])
+    paths["param"] = os.path.join(paths["main"], fldrs_main["param"])
+
+    conf = Map(paths)
     return conf
+
+
+if __name__ == "__main__":
+    get_config()
