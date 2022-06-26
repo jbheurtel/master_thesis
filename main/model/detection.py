12dbbb7dbb519d5c3f8a75418d3df462da13fd6b
jean-baptisteheurtel <jb.heurtel@gmail.com>
2022-06-26 15:50:46 +0200

    simplifying detection package

--- main/model/detection.py
+++ main/model/detection.py
@@ -1,6 +1,23 @@
-import pandas as pd
+import os
 from typing import List
+
+import numpy as np
+import pandas as pd
+import cv2
+from PIL import Image
+
+from main.parameters.base import ParamLoader
+
 from toolbox.file_manipulation.file import XmlFile, File
+from toolbox.config import get_config
+
+
+_COLOR_PALETTE = {
+    "house": (0, 255, 0),
+    "flooded_house": (0, 128, 255),
+    "damage": (255, 0, 0),
+    "destroyed_house": (64, 64, 64)
+}
 
 
 class Detection:
@@ -118,19 +135,67 @@ def get_detections_from_xml(xml_file: XmlFile):
     return detections
 
 
+def visualize(
+        image: np.ndarray,
+        detections: List[Detection],
+        margin=5,
+        row_size=15,
+        font_size=2,
+        font_thickness=2,
+        rectangle_thickness=2
+) -> np.ndarray:
+    for detection in detections:
+        # Draw bounding_box
+        start_point = detection.left, detection.top
+        end_point = detection.right, detection.bottom
+        # area = '{:0.2e}'.format(detection.area)
+        area = detection.area
+        item = detection.name
+        cv2.rectangle(image, start_point, end_point, _COLOR_PALETTE[item], rectangle_thickness)
 
-if __name__ == "__main__":
+        # Draw label and score
+        class_name = detection.name
+        probability = round(detection.score, 2)
+        result_text = class_name + ' (p: ' + str(probability) + ", area: " + str(area) + ')'
+        text_location = (margin + detection.right,
+                         margin + row_size + detection.top)
+        cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                    font_size, _COLOR_PALETTE[item], font_thickness)
 
-    all_detections = [
-        (1, 5, 4, 1, "house", 0.84),
-        (4, 8, 6, 3, "house", 0.34),
-        (3, 6, 5, 3, "damage", 0.59),
-        (1, 4, 3, 1, "damage", 0.94),
-        (7, 10, 7, 5, "damage", 0.30)
-    ]
+    return image
 
-    detections = DetectionSet([Detection(*i) for i in all_detections])
 
-    detections.summarise()
-    detections.get_unique_names()
+if __name__ == '__main__':
 
+    conf = get_config()
+    main = os.path.join(conf.data_annotated, "hurricanes_2")
+
+    all_paths = [os.path.join(main, i) for i in os.listdir(main)]
+    all_xml = [i for i in all_paths if File(i).extension == ".xml"]
+
+    XML_PATH = all_xml[2]
+
+    xml_file = XmlFile(XML_PATH)
+    image_path = XML_PATH.replace(".xml", ".png")
+    detections = get_detections_from_xml(xml_file=xml_file)
+
+    conf = get_config()
+    params = ParamLoader('6')
+
+    for d in detections:
+        d.relabel(params.label_map_dict)
+
+    image = Image.open(image_path).convert('RGB')
+    image_np_1 = np.asarray(image)
+    image_np = visualize(image_np_1, detections)
+
+    image = Image.fromarray(image_np)
+    image.show()
+
+    DX = DetectionSet(detections)
+    DX.get_unique_names()
+    DX.summarise()
+    groups = DX.form_groups()
+    groups_summary = summarise_groups(groups)
+    for i in groups_summary:
+        print(i)
